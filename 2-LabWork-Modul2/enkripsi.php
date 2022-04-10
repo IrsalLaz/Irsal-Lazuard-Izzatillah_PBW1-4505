b87b2b696de11c14de8d26f6350c36b7984e56a8
IrsalLaz <91246036+IrsalLaz@users.noreply.github.com>
2022-04-11 06:40:42 +0800

    update gost

--- 2-LabWork-Modul2/enkripsi.php
+++ 2-LabWork-Modul2/enkripsi.php
@@ -6,6 +6,6 @@
     echo sprintf("Kode enkripsi dari %s menggunakan MD5 adalah %s ", $user, md5($pass.$user));
     echo "<br>";
     echo sprintf("Kode enkripsi dari %s menggunakan sha1 adalah %s ", $user, sha1($pass.$user));
-    echo "<br>";
-    echo sprintf("Kode enkripsi dari %s menggunakan gost adalah %s ", $user, hash('gost', $str.$user));
+    echo "<br>";     
+    echo sprintf("Kode enkripsi dari %s menggunakan gost adalah %s ", $pass, hash('gost', $pass.$user));
 ?>
